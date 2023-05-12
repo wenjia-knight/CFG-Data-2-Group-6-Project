a0b63b73e9cea5ec924fc4800d9f974e626cee34
Wenjia Knight <112980096+wk7230545@users.noreply.github.com>
2023-05-12 21:57:13 +0100

    added SQL database of tracks

--- SQL database/sample_queries.sql
+++ SQL database/sample_queries.sql
@@ -19,65 +19,65 @@ FROM
  
 
 -- Most popular 40 songs that appeared on the charts for most amount of weeks:
-SELECT artist, title, COUNT(*) AS weeks_on_chart, f.mood
+SELECT COUNT(*) AS weeks_on_chart, artist, title
 FROM tracks_in_charts as tr
 LEFT JOIN feature_table as f
 ON tr.isrc = f.isrc
-GROUP BY 1, 2
-ORDER BY 3 DESC
-LIMIT 40; 
+GROUP BY 2, 3
+ORDER BY 1 DESC
+LIMIT 40;
 
--- What are the songs from WEEKEND that has been in chart in the past five years?
-SELECT COUNT(*) AS weeks_on_chart, title, artist, isrc 
+-- What are the songs from WEEKND that has been in chart in the past five years?
+SELECT COUNT(*) AS weeks_on_chart, title, artist, isrc
 FROM tracks_in_charts
 WHERE artist LIKE '%WEEKND%'
-GROUP BY 2
+GROUP BY 2, 3, 4
 ORDER BY 1 DESC;
 
--- 10 most popular artists in the past five years
+-- 10 most popular artists in the past five years by number of songs in charts
 SELECT artist, COUNT(DISTINCT title) AS number_of_songs
 FROM tracks_in_charts
 GROUP BY 1
 ORDER BY 2 DESC
 LIMIT 10;
- 
--- Taylor Swift songs that have been in charts 
-SELECT COUNT(*) AS weeks_on_chart, title, artist, isrc 
+
+-- Taylor Swift songs that have been in charts
+SELECT COUNT(*) AS weeks_on_chart, title, artist, isrc
 FROM tracks_in_charts
 WHERE artist LIKE '%TAYLOR SWIFT%'
-GROUP BY 2
+GROUP BY 2, 3, 4
 ORDER BY 1 DESC;
 
--- Ariana Grande songs that have been in charts 
-SELECT COUNT(*) AS weeks_on_chart, title, artist, isrc 
+-- Ariana Grande songs that have been in charts
+SELECT COUNT(*) AS weeks_on_chart, title, artist, isrc
 FROM tracks_in_charts
 WHERE artist LIKE '%ARIANA GRANDE%'
-GROUP BY 2
+GROUP BY 2, 3, 4
 ORDER BY 1 DESC;
 
 -- Ed Sheeran songs that have been in charts
-SELECT COUNT(*) AS weeks_on_chart, title, artist, isrc 
+SELECT COUNT(*) AS weeks_on_chart, title, artist, isrc
 FROM tracks_in_charts
 WHERE artist LIKE '%ED SHEERAN%'
-GROUP BY 2
+GROUP BY 2, 3, 4
 ORDER BY 1 DESC;
 
 -- 40 most popular song during first lock down
-SELECT artist, title, COUNT(*) AS weeks_on_chart, f.mood
+SELECT COUNT(*) AS weeks_on_chart, artist, title, f.mood
 FROM tracks_in_charts as tr
 LEFT JOIN feature_table as f
 ON tr.isrc = f.isrc
 WHERE dates BETWEEN '2020-03-20' AND '2020-06-26'
-GROUP BY 1, 2
-ORDER BY 3 DESC
-LIMIT 40; 
+GROUP BY 2, 3, 4
+ORDER BY 1 DESC
+LIMIT 40;
 
 -- 40 most popular song during second lock down
-SELECT artist, title, COUNT(*) AS weeks_on_chart, f.mood
+SELECT COUNT(*) AS weeks_on_chart, artist, title, f.mood
 FROM tracks_in_charts as tr
 LEFT JOIN feature_table as f
 ON tr.isrc = f.isrc
 WHERE dates BETWEEN '2020-10-30' AND '2020-12-04'
-GROUP BY 1, 2
-ORDER BY 3 DESC
+GROUP BY 2, 3, 4
+ORDER BY 1 DESC
 LIMIT 40; 

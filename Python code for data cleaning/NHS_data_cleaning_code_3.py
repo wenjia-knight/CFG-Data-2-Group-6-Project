dee1ee1941a8e42b7c4a8e293ca4c6550bce01db
tum-wade <autumn.ripley.wade@gmail.com>
2023-05-16 18:17:11 +0100

    finished nhs jupyter notebooks and uploaded all data/images to be used in the notebooks

--- Python code for data cleaning/NHS_data_cleaning_code_3.py
+++ Python code for data cleaning/NHS_data_cleaning_code_3.py
@@ -3,7 +3,6 @@ import pandas as pd
 
 
 def clean_nhs_data(data):
-    global CLEANED_DF
     data_copy = data.copy()
 
     # drop irrelevant columns
@@ -11,20 +10,23 @@ def clean_nhs_data(data):
 
     # rename columns
     data_copy.rename(columns={'REPORTING_PERIOD_START': 'month', 'MEASURE_ID': 'id',
-                              'MEASURE_VALUE': 'new_referrals_sum'}, inplace=True)
+                              'MEASURE_VALUE': 'new_referrals'}, inplace=True)
 
     # convert datatypes to strings
-    data_copy[['month', 'id']] = data_copy[['month', 'id']].astype(
+    data_copy[['month', 'id', 'new_referrals']] = data_copy[['month', 'id', 'new_referrals']].astype(
         "string")
 
     # remove * values
-    data_copy = data_copy[~data_copy['new_referrals_sum'].isin(['*'])]
+    data_copy = data_copy[~data_copy['new_referrals'].isin(['*'])]
 
     # convert new_referrals_sum to float
-    data_copy['new_referrals_sum'] = data_copy['new_referrals_sum'].astype("float")
+    data_copy['new_referrals'] = data_copy['new_referrals'].astype("float")
 
     # convert month column to date
     data_copy['month'] = data_copy['month'].apply(pd.to_datetime, format='%d/%m/%Y')
+
+    # take just the first row
+    global CLEANED_DF
 
     # filter by date range that we need
     date_filtered_df = data_copy.loc[(data_copy['month'] >= '2018-01-01')
@@ -37,13 +39,12 @@ def clean_nhs_data(data):
     id_date_filtered_df = date_filtered_df.loc[(data_copy['id'] == 'MHS32')]
 
     # sum up the values to get the total per month
-    # having issues here because its not adding up again to what's on the power bi and i don't know why
-    month_filtered_series = id_date_filtered_df.groupby('month')['new_referrals_sum'].sum()
+    month_filtered_series = id_date_filtered_df.groupby('month')['new_referrals'].sum()
     CLEANED_DF = pd.DataFrame(month_filtered_series)
 
     return CLEANED_DF
 
 
-data = pd.read_csv("../datasets/MHSDS Time_Series_data_Apr_2016_JanPrf_2023.csv")
+data = pd.read_csv("datasets/NHS_raw/MHSDS Time_Series_data_Apr_2016_JanPrf_2023.csv")
 clean_nhs_data(data)
-CLEANED_DF.to_csv('datasets/NHS_data_cleaned.csv', encoding='utf-8')
+CLEANED_DF.to_csv('datasets/NHS_cleaned/NHS_data_cleaned.csv', encoding='utf-8')
--- Python code for data cleaning/nhs_manual_file_cleaning.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-
-data = pd.read_excel("datasets/NHS_manual_data.xlsx", sheet_name='Sheet1')
-data.to_csv('datasets/NHS_manual_data_cleaned.csv', encoding='utf-8')
-
